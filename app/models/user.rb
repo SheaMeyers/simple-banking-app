aeefe1c3452fb4468bed62c88d122e7a48c4250d
Shea-Personal <shea.meyers@gmail.com>
2018-10-20 13:16:48 +0200

    Add simple login function for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
 		self.password = Digest::SHA1.hexdigest(password)
 		super
 	end 
+
+	def self.login(name, password)
+		encrypted_password = Digest::SHA1.hexdigest(password)
+		user = User.find_by(name: name, password: encrypted_password)
+
+		if (user == nil)
+			puts "Warning: Incorrect name or password"
+		end
+
+		return user
+	end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,28 @@ class UserTest < ActiveSupport::TestCase
 
     assert_equal(encrypted_password, test_user.password)
   end
+
+  test "login returns user when name and password are correct" do
+    password = 'password'
+    created_user = User.create :name => 'test_name', :password => password
+    logged_in_user = User.login('test_name', password)
+
+    assert_equal(logged_in_user, created_user)
+  end
+
+  test "login returns nil when name is not correct" do
+    password = 'password'
+    created_user = User.create :name => 'test_name', :password => password
+    logged_in_user = User.login('random_name', password)
+
+    assert_nil(logged_in_user, created_user)
+  end
+
+  test "login returns nil when password is not correct" do
+    password = 'password'
+    created_user = User.create :name => 'test_name', :password => password
+    logged_in_user = User.login('test_name', 'random_password')
+
+    assert_nil(logged_in_user, created_user)
+  end
 end
