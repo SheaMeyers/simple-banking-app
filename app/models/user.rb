7856f9b3a0a4cc1afc126a9ac4c6218523539466
Shea-Personal <shea.meyers@gmail.com>
2018-10-20 12:09:39 +0200

    Encrypt user password when saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
 	has_one :account
+
+	def save
+		self.password = Digest::SHA1.hexdigest(password)
+		super
+	end 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
+require 'digest/sha1'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user password gets encrypted upon saving" do
+    password = 'password'
+    encrypted_password = Digest::SHA1.hexdigest(password)
+
+    test_user = User.create :name => 'test_name', :password => password
+
+    assert_equal(encrypted_password, test_user.password)
+  end
 end
