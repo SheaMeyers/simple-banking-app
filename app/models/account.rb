3c023c4dea52c6fce15a324d531b83e678b38bb0
Shea-Personal <shea.meyers@gmail.com>
2018-10-21 10:19:09 +0200

    Fix bug in setting account_id

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,8 +6,11 @@ class Account < ApplicationRecord
 
   def save
 		# Override the save method in order to set a random account_id
+		
+		if (not self.account_id.present?)
+			self.account_id = SecureRandom.uuid
+		end
 
-		self.account_id = SecureRandom.uuid
 		super
 	end 
 end
